b6cb8c68a55c9a995eb0266aae99b46f61134dd8
pmoieni <pmoieni06@gmail.com>
2022-09-27 01:42:50 +0330

    initial commit

---
+++ lua/craftzdog/maps.lua
@@ -0,0 +1,23 @@
+local keymap = vim.keymap
+
+keymap.set('n', 'x', '"_x')
+
+-- Increment/decrement
+keymap.set('n', '+', '<C-a>')
+keymap.set('n', '-', '<C-x>')
+
+-- Delete a word backwards
+keymap.set('n', 'dw', 'vb"_d')
+
+-- Select all
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- Save with root permission (not working for now)
+--vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
+
+-- New tab
+keymap.set('n', 'te', ':tabedit')
+-- Split window
+keymap.set('n', 'ss', ':split<Return><C-w>w')
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
