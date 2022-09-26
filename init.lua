f43cd272b7979518e506d6d857b529d20414f0e0
pmoieni <pmoieni06@gmail.com>
2022-09-27 01:44:52 +0330

    changed name

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
-require('craftzdog.base')
-require('craftzdog.highlights')
-require('craftzdog.maps')
-require('craftzdog.plugins')
+require('pmoieni.base')
+require('pmoieni.highlights')
+require('pmoieni.maps')
+require('pmoieni.plugins')
 
 local has = vim.fn.has
 local is_mac = has "macunix"
 local is_win = has "win32"
 
 if is_mac then
-  require('craftzdog.macos')
+  require('pmoieni.macos')
 end
 if is_win then
-  require('craftzdog.windows')
+  require('pmoieni.windows')
 end
