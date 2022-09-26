5fffe60b3c89c66a5e0713ee195c60723cb86b13
pmoieni <pmoieni06@gmail.com>
2022-09-27 02:25:28 +0330

    small change

--- after/plugin/lualine.rc.lua
+++ after/plugin/lualine.rc.lua
@@ -7,7 +7,7 @@ lualine.setup {
     theme = 'solarized_dark',
     section_separators = { left = '', right = '' },
     component_separators = { left = '', right = '' },
-    disabled_filetypes = {}
+    disabled_filetypes = { 'NvimTree' }
   },
   sections = {
     lualine_a = { 'mode' },
