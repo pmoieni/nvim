13405f275aacd6b0c5693cae8770f21d44ffecbc
pmoieni <pmoieni06@gmail.com>
2022-09-27 15:32:14 +0330

    lspsaga config

--- lua/pmoieni/maps.lua
+++ lua/pmoieni/maps.lua
@@ -1,23 +1,23 @@
-local keymap = vim.keymap
+local map = vim.keymap.set
 
-keymap.set('n', 'x', '"_x')
+map('n', 'x', '"_x')
 
 -- Increment/decrement
-keymap.set('n', '+', '<C-a>')
-keymap.set('n', '-', '<C-x>')
+map('n', '+', '<C-a>')
+map('n', '-', '<C-x>')
 
 -- Delete a word backwards
-keymap.set('n', 'dw', 'vb"_d')
+map('n', 'dw', 'vb"_d')
 
 -- Select all
-keymap.set('n', '<C-a>', 'gg<S-v>G')
+map('n', '<C-a>', 'gg<S-v>G')
 
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
 -- New tab
-keymap.set('n', 'te', ':tabedit')
+map('n', 'te', ':tabedit')
 -- Split window
-keymap.set('n', 'ss', ':split<Return><C-w>w')
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+map('n', 'ss', ':split<Return><C-w>w')
+map('n', 'sv', ':vsplit<Return><C-w>w')
 
