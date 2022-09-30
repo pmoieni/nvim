e56ebcfcee54e1d3574fe758a97cc7fdb24ca18c
pmoieni <pmoieni06@gmail.com>
2022-09-30 12:55:53 +0330

    small fixes

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -9,10 +9,12 @@ saga.init_lsp_saga({
 	},
 })
 
+local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
-vim.keymap.set("n", "gd", "<Cmd>Lspsaga lsp_finder<CR>", opts)
-vim.keymap.set("i", "<C-k>", "<Cmd>Lspsaga signature_help<CR>", opts)
-vim.keymap.set("n", "gp", "<Cmd>Lspsaga preview_definition<CR>", opts)
-vim.keymap.set("n", "gr", "<Cmd>Lspsaga rename<CR>", opts)
+
+map("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+map("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
+map("n", "gd", "<Cmd>Lspsaga lsp_finder<CR>", opts)
+map("i", "<C-k>", "<Cmd>Lspsaga signature_help<CR>", opts)
+map("n", "gp", "<Cmd>Lspsaga preview_definition<CR>", opts)
+map("n", "gr", "<Cmd>Lspsaga rename<CR>", opts)
